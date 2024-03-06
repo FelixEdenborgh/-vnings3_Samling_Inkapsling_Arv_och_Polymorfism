55a5a7987b8ed316870fc40bacdf3f8aedef5f97
FelixEdenborgh <Felix@edenborgh.se>
2024-03-06 16:06:27 +0100

    Add project files.

---
+++ Övnings3_Samling_Inkapsling_Arv_och_Polymorfism/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Övnings3_Samling_Inkapsling_Arv_och_Polymorfism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
