a88c363d644eafdfb5fded656780f0c648c7b439
FelixEdenborgh <Felix@edenborgh.se>
2024-03-07 10:21:28 +0100

    Har skapat en abstract klass vid namn UserError Samt en abstrakt metod i klassen
    Har skapat 2 vanliga klasser som har metoder i sig som overridar den abstrakta klassen.
    Och som returnerar en text string.
    
    Har sedan skapat en lista i Program.cs
    Som har den abstrakta klassens namn som i fyllnad i listan. Och skapat instancer och lagt in dem i listan av de 2 klasserna som overridar den abstrikta klassen.
    
    Har sedan också skrivit ut allt som finns i listan.
    har också allt i en try catch block.

---
+++ Övnings3_Samling_Inkapsling_Arv_och_Polymorfism/UserError.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övnings3_Samling_Inkapsling_Arv_och_Polymorfism
+{
+    public abstract class UserError
+    {
+        public abstract string UEMessage();
+    }
+
+
+    public class NumericInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            return "You tried to use a\r\nnumeric input in a text only field. This fired an error!”";
+        }
+    }
+
+    public class TextInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            return "“You tried to use a text\r\ninput in a numeric only field. This fired an error!”";
+        }
+    }
+}
